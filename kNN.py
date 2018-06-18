c2a33571da6859d38c8b8d84e33387f88dd8bfa9
catherning <39702739+catherning@users.noreply.github.com>
2018-06-18 15:16:47 +0100

    First fitness function. It returns the 3rd smallest distance using unsupervised kNN.
    Higher the distance, higher the probability that the sequence represented by a feature vector is anomalous.

--- kNN.py
+++ kNN.py
@@ -8,8 +8,20 @@ import os
 from datetime import datetime
 from sklearn.neighbors import NearestNeighbors
 
+import numpy as np
+#import matplotlib.pyplot as plt
+#from matplotlib.colors import ListedColormap
+#from sklearn import neighbors
+
+
 path='D:/r6.2/users/'
 list_users=os.listdir(path)
+
+user=list_users[454]
+user_file=open(path+user)
+NB_NEIGHBORS=3
+
+
 
 """Reads the data in the csv file for one user, transforms it to a dictionary containing the values for the action"""
 def action(data):
@@ -45,7 +57,7 @@ def action(data):
     return action
 
 
-"""Returns the list of the days of activities. One day is represented by a featuer vector with:
+"""Returns the list of the days of activities. One day is represented by a feature vector with:
     - hour of beginning of the day
     - duration of the day (until last action done in the day)
     - number of logons/logoffs
@@ -82,28 +94,64 @@ def days(list_actions):
 
     return days
 
-if __name__ == "__main__":
-    user=list_users[454]
-    
-    user_file=open(path+user)
-    list_actions=[]
-    for line in user_file:
-        data=line.split(',')
-        activity=action(data)
-        list_actions.append(activity)
+#Takles the activity from the file of one user, combine it in one feature vector
+list_actions=[]
+for line in user_file:
+    data=line.split(',')
+    activity=action(data)
+    list_actions.append(activity)
 #        if data[0]=='t':~check if there's a \n
 #            print(data)
 #            print(activity)
-    #print(list_actions[0:10])
-    #CHECK IF IT IS WORKING
-    list_actions.sort(key=lambda r: r["date"])   #sort the sequences by date of action 
-    #print(list_actions[0:10])
-    #print(list_actions)
-    days=days(list_actions)
-    #print(days)
+#print(list_actions[0:10])
+#CHECK IF IT IS WORKING
+list_actions.sort(key=lambda r: r["date"])   #sort the sequences by date of action 
+#print(list_actions[0:10])
+#print(list_actions)
+days=days(list_actions)
+X=np.asarray(days)
+
+
+#kNN Unsupervised
+nbrs = NearestNeighbors(n_neighbors=NB_NEIGHBORS, algorithm='kd_tree').fit(X)   #kd_tree fastest
+
+"""
+Fitness for GA. Evaluate if the feature vector is anomalous or not
+"""
+def fitness(individual):
+    #Supervised kNN
+#    y=np.zeros(len(X))
+#    y[0]=1
+#    iris = datasets.load_iris()
+#    X = iris.data[:, :2]
+#    y = iris.target
+#    
+#    n_neighbors = 15
+#
+#    for weights in ['uniform', 'distance']: 
+#        # we create an instance of Neighbours Classifier and fit the data.
+#        clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+#        clf.fit(X, y)
+#    
+#        Z = clf.predict([X[0,:]])
+#        print(Z)
+        
+        
+    #Unsupervised kNN
+    distances, indices = nbrs.kneighbors([individual])
+    print(distances[0,2]) 
+    #print(indices)
+    return distances[0,2]  
+
+if __name__ == "__main__":
+
     
-    nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(days)
-    distances, indices = nbrs.kneighbors(days)
-    print(indices) 
+
+#    
+    ind1=[0.03,1.0,0.0,0.002,0,0]
+    ind2=[0.05,1.0,0.0,0.04,0,0]
+    fitness(ind1)
+    fitness(ind2)
+
 
 
